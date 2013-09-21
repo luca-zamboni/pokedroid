9622de68ad616279b11073c7e972ff775b943618
jibbo <posta.giovanni.defracesco@gmail.com>
2013-09-21 03:24:35 +0200

    ok... riescoa  far apparire roba ad una cerca coordinata

--- src/it/pokefundroid/pokedroid/Splash_activity.java
+++ src/it/pokefundroid/pokedroid/Splash_activity.java
@@ -3,6 +3,7 @@ package it.pokefundroid.pokedroid;
 import it.pokefundroid.pokedroid.utils.FindingUtilities;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,68 +12,60 @@ import android.view.Menu;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Splash_activity extends Activity { 
-	
+public class Splash_activity extends Activity {
 
 	public TextView text;
 	int ratata = 0;
+	private LocationManager locationManager;
+	private LocationListener locationListener;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_activity);
-        final int[] pokemon = new int[151];
-        
-        for(int i = 0; i < 151; i++) {
-        	pokemon[i]=i;
-        }
-       
-        LocationManager locationManager = (LocationManager) 
-        		getSystemService(Context.LOCATION_SERVICE);
-        LocationListener locationListener = new LocationListener() {
-			
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash_activity);
+
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		locationListener = new LocationListener() {
+
 			@Override
-			public void onStatusChanged(String provider, int status, Bundle extras) {
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
 			}
-			
+
 			@Override
 			public void onProviderEnabled(String provider) {
 			}
-			
+
 			@Override
 			public void onProviderDisabled(String provider) {
 			}
-			
+
 			@Override
 			public void onLocationChanged(Location location) {
 				
-				int condition = FindingUtilities.findInGroup(pokemon);
-				setText(location.getLatitude() + " " + location.getLongitude() + " " + condition+" "+FindingUtilities.getRarityFromId(condition));
-				if(condition >= 0){
-					ratata++;
-					Toast.makeText(Splash_activity.this, "Hai trovato "+ratata+" pokemon, id: " 
-					+condition+1, 1000).show();
-				}
+				locationManager.removeUpdates(locationListener);
+				Splash_activity.this.finish();
 			}
 		};
-        locationManager.requestLocationUpdates(  
-        LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
-        
-        //Intent newActivity = new Intent(Splash_activity.this, Sprite_Activity.class);
-        //startActivity(newActivity);
-    }
+		/*locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				5000, 10, locationListener);*/
+		Intent newActivity = new Intent(Splash_activity.this,
+				Sprite_Activity.class);
+		//newActivity.putExtra("loc", new double[]{location.getLatitude(),location.getLongitude(),location.getAltitude()});
+		startActivity(newActivity);
+		Splash_activity.this.finish();
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.splash_activity, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.splash_activity, menu);
-        return true;
-    }
-    
-    public void setText(String m){ 
+	public void setText(String m) {
 		text = (TextView) findViewById(R.id.text);
 		text.setText(m);
-    }
-    
+	}
+
 }
---
+++ src/it/pokefundroid/pokedroid/Sprite_Activity.java
@@ -0,0 +1,122 @@
+package it.pokefundroid.pokedroid;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import com.beyondar.android.opengl.util.FpsUpdatable;
+import com.beyondar.android.opengl.views.BeyondarGLSurfaceView;
+import com.beyondar.android.opengl.views.OnARTouchListener;
+import com.beyondar.android.views.CameraView;
+import com.beyondar.android.world.World;
+import com.beyondar.android.world.objects.GeoObject;
+import com.beyondar.android.world.objects.BeyondarObject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.widget.Toast;
+
+public class Sprite_Activity extends Activity implements OnARTouchListener {
+
+	private BeyondarGLSurfaceView mBeyondarGLSurfaceView;
+	private CameraView mCameraView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_sprite);
+		
+		mBeyondarGLSurfaceView = (BeyondarGLSurfaceView) findViewById(R.id.customGLSurface);
+		mCameraView = (CameraView) findViewById(R.id.camera);
+
+		// We create the world and set it in to the view
+		World world = createWorld();
+		mBeyondarGLSurfaceView.setWorld(world);
+		// set listener for the geoObjects
+		mBeyondarGLSurfaceView.setOnARTouchListener(this);
+	}
+
+	private World createWorld() {
+		World w = new World(this);
+		
+//		double[] loc = getIntent().getExtras().getDoubleArray("loc");
+//		w.setLatitude(loc[0]);
+//		w.setLongitude(loc[1]);
+//		w.setAltitude(loc[2]);
+//		
+//		go1.setLatitude(loc[0]);
+//		go1.setLongitude(loc[1]);
+//		go1.setAltitude(loc[2]);
+		w.setLongitude(1.925848038959814d);
+		w.setLatitude(41.26533734214473d);
+		
+		GeoObject go1 = new GeoObject(4l);
+		go1.setLongitude(1.925662767707665d);
+		go1.setLatitude(41.26518862002349d);
+		go1.setImageUri("assets://charmender.png");
+		go1.setName("Image from assets");
+		
+		w.addBeyondarObject(go1);
+		w.setDefaultBitmap(R.drawable.creature_6);
+
+		return w;
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mBeyondarGLSurfaceView.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mBeyondarGLSurfaceView.onPause();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.sprite_, menu);
+		return true;
+	}
+
+	@Override
+	public void onTouchARView(MotionEvent event,
+			BeyondarGLSurfaceView beyondarView) {
+
+		float x = event.getX();
+		float y = event.getY();
+
+		ArrayList<BeyondarObject> geoObjects = new ArrayList<BeyondarObject>();
+
+		beyondarView.getARObjectOnScreenCoordinates(x, y, geoObjects);
+
+		String textEvent = "";
+
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			textEvent = "Event type ACTION_DOWN: ";
+			break;
+		case MotionEvent.ACTION_UP:
+			textEvent = "Event type ACTION_UP: ";
+			break;
+		case MotionEvent.ACTION_MOVE:
+			textEvent = "Event type ACTION_MOVE: ";
+			break;
+		default:
+			break;
+		}
+
+		Iterator<BeyondarObject> iterator = geoObjects.iterator();
+		while (iterator.hasNext()) {
+			BeyondarObject geoObject = iterator.next();
+			textEvent = textEvent + " " + geoObject.getName();
+
+		}
+	}
+
+}
