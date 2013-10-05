050090afd1cfd5dee500c3af9c98b0b54f48cba1
luca-zamboni <lucazamboni92@gmail.com>
2013-10-05 17:01:00 +0200

    Fixed add personal pokemon to database

--- src/it/pokefundroid/pokedroid/models/PersonalPokemon.java
+++ src/it/pokefundroid/pokedroid/models/PersonalPokemon.java
@@ -53,6 +53,12 @@ public class PersonalPokemon extends Pokemon {
 				BaseHelper.SEX+"," +
 				BaseHelper.FOUND_X+"," +
 				BaseHelper.FOUND_Y+"," +
+				BaseHelper.HPEV+"," +
+				BaseHelper.ATKEV+"," +
+				BaseHelper.DEFEV+"," +
+				BaseHelper.SATKEV+"," +
+				BaseHelper.SDEFEV+"," +
+				BaseHelper.SPDEV+"," +
 				BaseHelper.MY_NAME+" ) ";
 		int sex;
 		if(this.sex == PokemonSex.MALE)
@@ -66,6 +72,12 @@ public class PersonalPokemon extends Pokemon {
 				sex +"," +
 				found_x +"," +
 				found_y +"," +
+				hpEv +"," +
+				atkEv +"," +
+				defEv +"," +
+				sAtkEv +"," +
+				sDefEv +"," +
+				spdEv +"," +
 				"'"+my_name +"'); "  ;
 		
 		Log.e("asd", insertPersonalPokemon);
